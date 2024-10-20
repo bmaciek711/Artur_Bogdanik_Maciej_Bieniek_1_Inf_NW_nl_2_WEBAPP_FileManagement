0ad9787e7e7998b6d00e57729d0506e29ce78a6f
Bartosz 147725 <chx102562@student.chorzow.merito.pl>
2024-10-20 09:55:20 +0200

    initail commit

---
+++ FileManagmentApp/Models/FileModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace FileManagmentApp.Models
+{
+    public class FileModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string FileName { get; set; }
+
+        public string FilePath { get; set; }
+
+        public DateTime UploadDate { get; set; }
+    }
+}
